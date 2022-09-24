fdaaaf54a0e0016927a1f8d76ce37d3a186cb6ff
jyohub <jyohub@icloud.com>
2022-09-24 16:16:13 +0900

    LearnJava - First Commit

---
+++ src/main/java/org/javalearning/LearnTypes.java
@@ -0,0 +1,18 @@
+package org.javalearning;
+
+public class LearnTypes {
+
+    public static void main(String[] args) {
+        byte byteVal = 2;
+        short shortVal = 3;
+        int intVal = 5;
+        long longVal = 2000000;
+
+        float floatVal = 20.5555f;
+        double doubleVal = 30.5555555555;
+
+        System.out.println(longVal);
+        System.out.println(floatVal);
+        System.out.println(doubleVal);
+    }
+}
